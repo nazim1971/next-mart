1f418c97891ca0fecc8f05dbdbdae8eaaf1a47e5
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-24 23:45:18 +0600

    work on redirect path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,18 @@
-export const middleware =async () =>{
-    console.log("hello world");
+import { NextRequest, NextResponse } from "next/server";
+import { getCurrentUser } from "./services/AuthService"
+
+const authRoutes = ["/login", '/register']
+
+export const middleware =async  (req: NextRequest) =>{
+    const {pathname} = req.nextUrl;
+    const userInfo = await getCurrentUser();
+    if(!userInfo){
+        if(authRoutes.includes(pathname)){
+            return NextResponse.next()
+        } else{
+            return NextResponse.redirect(new URL(`http://localhost:3000/login?redirectPath=${pathname}`, req.url))
+        }
+    }
 }
 
 export const config = {
