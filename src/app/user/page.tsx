37a9455d93ea99759487369a76c9a09bb12dcd53
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-25 02:49:15 +0600

    add admin and user

---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserHomePage = () => {
+    return (
+        <div>
+            This is user home page.
+        </div>
+    );
+};
+
+export default UserHomePage;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getCurrentUser } from "./services/AuthService"
 
+type Role = keyof typeof roleBasedPrivateRoutes
+
 const authRoutes = ["/login", '/register']
+
+const roleBasedPrivateRoutes = {
+    user: [/^\/user/],
+    admin: [/^\/admin/]
+}
 
 export const middleware =async  (req: NextRequest) =>{
     const {pathname} = req.nextUrl;
@@ -11,6 +18,13 @@ export const middleware =async  (req: NextRequest) =>{
             return NextResponse.next()
         } else{
             return NextResponse.redirect(new URL(`http://localhost:3000/login?redirectPath=${pathname}`, req.url))
+        }
+    }
+
+    if(userInfo?.role && roleBasedPrivateRoutes[userInfo?.role as Role]){
+        const routes = roleBasedPrivateRoutes[userInfo?.role as Role];
+        if (routes.some(route=> pathname.match(route))){
+            return NextResponse.next();
         }
     }
 }
