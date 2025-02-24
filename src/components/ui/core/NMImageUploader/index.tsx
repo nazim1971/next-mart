4d7f35c9f6af8c99641fc20c96c99c30a5e69e73
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-24 18:29:03 +0600

    add reusable image previewer

--- src/components/ui/core/NMImageUploader/index.tsx
+++ src/components/ui/core/NMImageUploader/index.tsx
@@ -1,16 +1,35 @@
 import React, { useState } from "react";
 import { Input } from "../../input";
+import { cn } from "@/lib/utils";
 
-const NMImageUploader = () => {
-  const [imageFiles, setImageFiles] = useState<File[] | []>([]);
+type TImageUploaderProps = {
+    label?: string;
+  className?: string;
+  setImageFiles: React.Dispatch<React.SetStateAction<File[]>>;
+  setImagePreview: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const NMImageUploader = ({ label = "Upload Images",
+    className,
+    setImageFiles,
+    setImagePreview,
+  }: TImageUploaderProps) => {
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files![0];
     setImageFiles((p) => [...p, file]);
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview((p) => [...p, reader.result as string]);
+      };
+      reader.readAsDataURL(file);
+    }
+    e.target.value = "";
   };
 
   return (
-    <div>
+    <div className={cn("flex flex-col items-center  w-full gap-4",className)}>
       <Input
         onChange={handleImageChange}
         type="file"
@@ -23,8 +42,7 @@ const NMImageUploader = () => {
         htmlFor="image-uoloader"
         className="w-full h-36 md:size-36 flex items-center justify-center border-2 border-dashed border-gray-300 rounded-md cursor-pointer text-center text-sm text-gray-500 hover:bg-gray-50 transition"
       >
-        {" "}
-        Upload logo{" "}
+        {label}
       </label>
     </div>
   );
